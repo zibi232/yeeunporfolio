9b55c8de5ed87a3403255b42c1fc0d1fa0cda138
zibi232 <121073053+zibi232@users.noreply.github.com>
2023-08-10 12:47:22 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.element', {
+  strings: ["Artist.", "Stylist.", "Dancer.", "Influencer."],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+});
+// JavaScript Document
+$('.counter').countUp();
---
+++ js/slider.js
@@ -0,0 +1,8 @@
+$('#slider').owlCarousel({
+  loop: true, // cho lap lại
+  items: 1, // xuất hiện 1 ảnh thôi
+  smartSpeed: 1000, // tốc độ thay đổi ảnh
+  autoplay: true, // cho phép tự động chạy
+  autoplayTimeout: 5000, // thời gian chờ khi chuyển ảnh khi chạy tự động
+  nav: true, // cho xuất hiện bộ nút tới lui 
+})
